30887d5e65576e4a264c68d8d32c94b36f483c60
missmaheendanish <https://github.com/missmaheendanish/simple-calculator>
2024-03-27 11:20:45 +0500

    created number guessing game

---
+++ main.js
@@ -0,0 +1,20 @@
+#! /user/bin/env node
+import inquirer from "inquirer";
+// 1. computer will generate a random number
+// 2. user input for guessing number
+// 3. computer user input with computer generate number and show result - done
+const randomNumber = Math.floor(Math.random() * 10 + 1);
+console.log(randomNumber);
+const answers = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type: "number",
+        message: "please guess a number between 1-10: ",
+    },
+]);
+if (answers.userGuessedNumber === randomNumber) {
+    console.log("congratulations! you gussed right number.");
+}
+else {
+    console.log("you guessed wrong number.");
+}
